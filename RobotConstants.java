3bb6808241b71896ecf2e0140f32f828f2729969
Malachi Bergman <mbergman@malachijb.com>
2025-01-29 19:10:40 -0500

    tune cancoder offsets for pbot swerve

--- RobotConstants.java
+++ RobotConstants.java
@@ -18,10 +18,10 @@ public class RobotConstants {
                 .withKP(0.1).withKI(0).withKD(0)
                 .withKS(0.1566).withKV(2.3096).withKA(0.0232);
 
-        public static final Angle FRONT_LEFT_ENCODER_OFFSET = Rotations.of(0.145751953125);
-        public static final Angle FRONT_RIGHT_ENCODER_OFFSET = Rotations.of(-0.001953125);
-        public static final Angle BACK_LEFT_ENCODER_OFFSET = Rotations.of(0.326416015625);
-        public static final Angle BACK_RIGHT_ENCODER_OFFSET = Rotations.of(0.22998046875);
+        public static final Angle FRONT_LEFT_ENCODER_OFFSET = Rotations.of(0.27392578125);
+        public static final Angle FRONT_RIGHT_ENCODER_OFFSET = Rotations.of(0.237548828125);
+        public static final Angle BACK_LEFT_ENCODER_OFFSET = Rotations.of(-0.123779296875);
+        public static final Angle BACK_RIGHT_ENCODER_OFFSET = Rotations.of(0.44580078125);
 
     }
 }
